4af79e398c4c8865111ea0cc3471a2af59eec2ba
Derryl999 <derryl.aranha999@gmail.com>
2021-09-16 17:43:53 +0530

    setting up git

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>hello</p>
+      <p>hii</p>
+      
+    </div>
+  );
+}
+
+export default App;
