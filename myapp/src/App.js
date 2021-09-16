c8f3290ff98b028dd93307faadd7e623510069d4
Derryl999 <derryl.aranha999@gmail.com>
2021-09-16 20:38:59 +0530

    frontend

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
-
+import {Container,Typography,Grow,AppBar,Grid} from '@material-ui/core'
+import memories from './images/memories.png' 
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './components/styles'
 function App() {
+  const classes=useStyles()
   return (
     <div>
-      <h1>Hello</h1>
-      <p>hello</p>
-      <p>hii</p>
-      
+<Container maxWidth="lg">
+  <AppBar className={classes.appBar} position="static" color="inherit">
+    <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+    <img  className={classes.image} src={memories} alt="memories" height="60" />
+  </AppBar>
+  <Grow in>
+    <Container>
+      <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+        <Grid xs="12" sm="7">
+        <Posts/>
+        </Grid>
+        <Grid xs="12" sm="4">
+        <Form/>
+        </Grid>
+
+      </Grid>
+    </Container>
+  </Grow>
+  </Container>      
     </div>
   );
 }
--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -2,8 +2,9 @@ import Post from '../models/postModel.js'
 export const getPosts=(req,res)=>{
     Post.find()
     .then((result)=>{
-        
+        res.send(result)
     })
+    .catch(err=>console.log(err))
 }
 
 export const createPost=(req,res)=>{
